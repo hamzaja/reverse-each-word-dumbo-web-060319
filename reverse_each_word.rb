2f8733b2db79c86c5c637c755064f67dd95cbb17
HAMZA Javed <hamzajaved71@gmail.com>
2019-05-26 07:15:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,8 @@
 def reverse_each_word(a)
 a = a.split(" ")
 b = []
-  original_array.each do|string|
-    return_array << string.reverse
+a.each do|x|
+    b << x.reverse
   end
   b.join(" ")
-end return a.reverse_each {|x| x }
-end
+end 
