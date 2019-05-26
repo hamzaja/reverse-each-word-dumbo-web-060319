b404f9a936f3e1509a05e757400ec6128b7649d2
HAMZA Javed <hamzajaved71@gmail.com>
2019-05-26 07:15:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,9 @@
 def reverse_each_word(a)
 a = a.split(" ")
-return a.reverse_each {|x| x }
+b = []
+  original_array.each do|string|
+    return_array << string.reverse
+  end
+  b.join(" ")
+end return a.reverse_each {|x| x }
 end
